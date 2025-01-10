9418454074d684f37e6fb51a035122bb7d3395aa
HOTPOT <tpcmaky@gmail.com>
2025-01-10 10:45:58 +0100

    removed useless API route

--- app/api/github-activity/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-import { getGithubActivity } from '@/lib/github';
-
-export async function GET() {
-  const username = 'HOTP0T'; // Replace with your GitHub username
-  const activity = await getGithubActivity(username);
-  return NextResponse.json(activity);
-}

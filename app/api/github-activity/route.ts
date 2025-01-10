b49b3d726f0378a6c015779ccc925a0d434d32cd
HOTPOT <tpcmaky@gmail.com>
2025-01-10 10:38:29 +0100

    fixing api routing for deployment

--- app/api/github-activity/route.ts
+++ app/api/github-activity/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { getGithubActivity } from '@/lib/github';
 
 export async function GET() {
-  const username = 'yourgithubusername'; // Replace with your GitHub username
+  const username = 'HOTP0T'; // Replace with your GitHub username
   const activity = await getGithubActivity(username);
   return NextResponse.json(activity);
 }
